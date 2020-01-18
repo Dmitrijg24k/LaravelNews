2864fe0e445e7c4a323df7de7e6e10f25207f715
Dmitrijg24k <dmitrij3e5g@gmail.com>
2020-01-18 10:20:27 +0400

    display categories and news | metadata

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,10 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>@yield('title')</title>
+    <meta name="keyword" content="@yield('mete_keyword')">     
+    <meta name="description" content="@yield('mete_description')">   
+   
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
